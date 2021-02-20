bfa69c0796f71daefc528d90bb1d23d82d8c1c15
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-02-20 19:04:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var config = {
           this.load.image('ground', 'assets/platform.png');
           this.load.image('star', 'assets/star.png');
           this.load.image('bomb', 'assets/bomb.png');
-          this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+          this.load.spritesheet('dude', 'chalkers.png', { frameWidth: 48, frameHeight: 62 });
     }
 
 
@@ -50,24 +50,22 @@ var config = {
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
 
-        this.anims.create({
-          key: 'left',
-          frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-          frameRate: 10,
-          repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
-            frameRate: 20
-        });
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
+        // is the left cursor key presssed?
+    if (cursors.left.isDown) {
+      player.animations.play('walk', 10, true);
+      player.body.velocity.x = -300;
+      player.scale.x = - 1;
+    }
+    // is the right cursor key pressed?
+    else if (cursors.right.isDown) {
+      player.animations.play('walk', 10, true);
+      player.body.velocity.x = 300;
+      player.scale.x = 1;
+    }
+    // player doesn't move
+    else {
+      player.animations.stop();
+    }
 
         cursors = this.input.keyboard.createCursorKeys();
 
